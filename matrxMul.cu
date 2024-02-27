f20390b42038331e2a2ccb6924b9829068b31bd6
weigangqiang <weigangqiang@lixiang.com>
2024-02-27 14:30:59 +0800

    init

---
+++ matrxMul.cu
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <cuda_runtime.h>
+
+// // CUDA Kernel function to add elements of two arrays
+// __global__ void vector_add(const float *A, const float *B, float *C, int numElements) {
+//     int i = blockDim.x * blockIdx.x + threadIdx.x;
+//     if (i < numElements) {
+//         C[i] = A[i] + B[i];
+//     }
+// }
+
+__global__ void TccCalcNonSparse(int8_t* ab, int8_t* wb, int32_t* partial_sum){
+    int matrx_idx = blockDim.x * blockIdx.x + threadIdx.x;
+    for (size_t m = 0; m != 32; ++m) {
+      for(size_t n = 0; n != 64; ++n){
+         partial_sum[matrx_idx * 2048 + m * 64 + n] = 0;
+      }  
+      for (size_t k = 0; k != 32; ++k) {
+        int8_t tmp = ab[matrx_idx *1024 +m*32 +k];
+        for (size_t n = 0; n != 64; ++n) {
+          partial_sum[matrx_idx * 2048 + m * 64 + n] += tmp * wb[matrx_idx *2048 + k* 64 + n];
+        }
+      }
+    }
+}
+
+// __global__ void TccCalcNonSparse(int8_t* ab, int8_t* wb, int32_t* partial_sum){
+//     int matrx_idx = blockDim.x * blockIdx.x + threadIdx.x;
+//     for (size_t m = 0; m != 32; ++m) {
+//       for (size_t k = 0; k != 32; ++k) {
+//         int8_t tmp = ab[matrx_idx *1024 +m*32 +k];
+//         for (size_t n = 0; n != 64; ++n) {
+//           partial_sum[m][n] += tmp * wb[k][n];
+//         }
+//       }
+//     }
+// }
+
+
+int main(void) {
+    int batchSize = 16;
+    int abElements = 1024 * batchSize;
+    int wbElements = 2048 * batchSize;
+    int sumElements = 2048 * batchSize;
+    
+    int8_t *h_ab, *h_wb, *d_ab, *d_wb;
+    int32_t *h_sum, *d_sum;
+
+    // Allocate host memory
+    h_ab = (int8_t *)malloc(abElements);
+    h_wb = (int8_t *)malloc(wbElements);
+    h_sum = (int32_t *)malloc(sumElements * sizeof(int32_t));
+
+
+    // Allocate device memory
+    cudaMalloc((void **)&d_ab, abElements);
+    cudaMalloc((void **)&d_wb, wbElements);
+    cudaMalloc((void **)&d_sum, sumElements * sizeof(int32_t));
+
+    // Copy data from host to device
+    cudaMemcpy(d_ab, h_ab, abElements, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_wb, h_wb, wbElements, cudaMemcpyHostToDevice);
+
+    // Launch the Vector Add CUDA Kernel
+    int threadsPerBlock = 16;
+    int blocksPerGrid = 1;
+    TccCalcNonSparse<<<blocksPerGrid, threadsPerBlock>>>(d_ab, d_wb, d_sum);
+
+    // Copy result back to host
+    cudaMemcpy(h_sum, d_sum, sumElements * sizeof(int32_t), cudaMemcpyDeviceToHost);
+
+
+    printf("Test PASSED\n");
+
+    // Free device memory
+    cudaFree(d_ab);
+    cudaFree(d_wb);
+    cudaFree(d_sum);
+
+    // Free host memory
+    free(h_ab);
+    free(h_wb);
+    free(h_sum);
+
+    return 0;
+}
---
+++ readme.md
@@ -0,0 +1,24 @@
+
+
+/usr/local/cuda-11.4/bin
+
+export env
+
+```
+export PATH=/usr/local/cuda-11.4/bin:$PATH
+export LD_LIBRARY_PATH=/usr/local/cuda-11.4/lib64:$LD_LIBRARY_PATH
+
+```
+
+
+build 
+
+```
+nvcc matrxMul.cu -o matrxMul
+
+
+```
+
+- cyrus
+- 123456
+
